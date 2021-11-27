62fa3d709ebca99698974ea2305050364f791ff0
Aleksandra <aleksandra.kuper@wp.pl>
2021-11-27 11:04:18 +0100

    Creating

---
+++ Models/Localization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZnajdzLekarza.Models
+{
+    public class Localization
+    {
+        public int Id { get; set; }
+        
+        public string StreetName { get; set; }
+
+        public string StreetNumber { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ Models/Specialist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZnajdzLekarza.Models
+{
+    public class Specialist
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public string Education { get; set; }
+
+        public List<Localization> Localizations { get; set; }
+
+        public List<Specialization> Specializations { get; set; }
+
+        public List<Service> Services { get; set; }
+    }
+}
