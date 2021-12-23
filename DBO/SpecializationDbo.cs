0711e5f0b6122b8081b35005bad566f094bec73a
Aleksandra <aleksandra.kuper@wp.pl>
2021-12-23 20:45:00 +0100

    Creating DBO Objects - Code First

---
+++ DBO/SpecializationDbo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZnajdzLekarza.DBO
+{
+    public class SpecializationDbo
+    {
+        public int ID { get; set; }
+
+        public int SpecialistDboID { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual SpecialistDbo SpecialistDbo { get; set; }
+    }
+}
