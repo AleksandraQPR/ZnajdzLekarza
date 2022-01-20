4107405af9be6475a8544808982bd8c45f5487e9
Aleksandra <aleksandra.kuper@wp.pl>
2022-01-20 09:40:03 +0100

    New DBContext class and adding dbContext service to Startap class

---
+++ DBO/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZnajdzLekarza.DBO
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Localization> Localizations { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Specialist> Specialists { get; set; }
+        public DbSet<Specialization> Specializations { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ZnajdzLekarza.DBO;
 
 namespace ZnajdzLekarza
 {
@@ -20,6 +22,9 @@ namespace ZnajdzLekarza
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>
+                (options => options.UseMySQL(Configuration.GetConnectionString("Default")));
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
